f235f63249d6f828e30f29fe9f5ffee75cf6f5d3
Seokwoodang <swoo1429@naver.com>
2024-03-07 13:19:33 +0900

    StoryBook 재생성 및 디자인 시스템 적용중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
-import CheckBox from "./components/CheckBox";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [checkBoxDisabled, setCheckBoxDisabled] = useState<boolean>(false);
-  const [checked, setChecked] = useState<boolean>(false);
-
   return (
     <div className="App">
-      <CheckBox checked={checked} onClick={() => setChecked(!checked)} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
